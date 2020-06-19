c9879163f543eb164e346b803d6f7ad6278bc82e
Rameez Khan <rameez.ka.khan@bmwgroup.com>
2020-06-19 15:10:52 +0200

    Add code

--- src/scrabble/core.clj
+++ src/scrabble/core.clj
@@ -1,7 +1,25 @@
 (ns scrabble.core
+  (:require [clojure.string :as str])
   (:gen-class))
+
+(defn scramble [word]
+  (->> word
+       seq
+       shuffle
+       (apply str)))
+
+(defn sentence-scramble [sentence]
+  (->> (str/split sentence #" ")
+       (map scramble)
+       (str/join " ")))
+
+(def sentence
+  "this is pure gibberish man")
+
+(sentence-scramble sentence)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Can you guess the sentence? \n"
+           (sentence-scramble sentence)))
